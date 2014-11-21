9c3cb57d1feff6665d9d66184f5e02efb032267e
Jhon Mike <jhonmike@hotmail.com.br>
2014-11-21 15:20:50 -0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+ApiAuth
+=======
+
+	php composer.phar require 2ends/apigility-auth:dev-master
---
+++ config/module.config.php
@@ -0,0 +1,7 @@
+<?php
+/**
+* @author Jhon Mike Soares <https://github.com/jhonmike>
+* @version 1.0
+*/
+
+return array();
